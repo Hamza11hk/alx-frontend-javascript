c2975af37a2c7ddd5b17e8e86145de955f543c00
Hamza11hk <Hamzahk110077@gmail.com>
2024-01-02 21:28:13 +0000

    change_to_const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
+  const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // let to create a block-scoped variable
-    const task2 = false;  // const to create a block-scoped constant
+    const task = true;
+    const task2 = false;
+    if (task && task2) {
+      return [task, task2];
+    }
   }
 
   return [task, task2];

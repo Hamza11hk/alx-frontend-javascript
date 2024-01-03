7e853409d86f0917bf80caf5706ffbf433d62577
Hamza11hk <Hamzahk110077@gmail.com>
2024-01-03 20:05:00 +0000

    remove const

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const [photoResponse, userResponse] = results;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
